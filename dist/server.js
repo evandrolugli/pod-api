07478cf6baea853145128ed8055393fbf832ab62
Evandro Lugli <evandro.lugli@gmail.com>
2024-06-27 20:29:24 +1000

    fix heroku

--- dist/server.js
+++ dist/server.js
@@ -5,15 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.startServer = void 0;
 const fastify_1 = __importDefault(require("fastify"));
-const prisma_1 = require("./lib/prisma");
+const cors_1 = __importDefault(require("@fastify/cors"));
 const app = (0, fastify_1.default)();
 const PORT = Number(process.env.PORT || 3000);
+app.register(cors_1.default, {
+    origin: '*', // Modify according to your needs
+});
 app.get('/', async (request, reply) => {
     return { message: 'Hello, Heroku with Fastify!' };
 });
 app.get('/prompts', async (request, reply) => {
-    const prompts = await prisma_1.prisma.prompt.findMany();
-    return prompts;
+    return { message: 'Hello Prompts!' };
+    //const prompts = await prisma.prompt.findMany()
+    //return prompts
 });
 function startServer(port = PORT) {
     app.listen({ port, host: '0.0.0.0' }, (err, address) => {
